6dd356c29805871f4b006e31560315d1425de343
Patrick Cook <cookpate@amazon.com>
2022-12-02 15:26:12 -0800

    add: safe enum casting

--- helper.hh
+++ helper.hh
@@ -1,6 +1,21 @@
+#ifndef AOC_2022_HELPER
+#define AOC_2022_HELPER
 
 #include <sstream>
 #include <filesystem>
 #include <optional>
+#include <algorithm>
+
 // read entire contents of a file into a string stream
 std::optional<std::istringstream> ReadFile(std::filesystem::path path);
+
+// casts int to enum, checking bad values
+template<typename Enum, Enum min, Enum max>
+constexpr std::optional<Enum> EnumCast(const int toCast) noexcept
+{
+    static_assert(min <= max, "Inverted bounds!");
+    Enum casted = std::max( min, std::min( static_cast<Enum>( toCast ), max ) );
+    return casted == toCast ? std::make_optional(casted) : std::nullopt;
+}
+
+#endif
