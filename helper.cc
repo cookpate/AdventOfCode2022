8ffb91cc480fb30040c8eb9ff0e5aa24ad77f0fb
Patrick Cook <cookpate@amazon.com>
2023-02-06 17:54:26 -0800

    add: day 4 helpers

--- helper.cc
+++ helper.cc
@@ -2,15 +2,23 @@
 
 #include <fstream>
 
-std::optional<std::istringstream> ReadFile(const std::filesystem::path path)
-{
-    std::ifstream file(path);
-    if (!file)
-    {
-        return std::nullopt;
-    }
-    std::string buffer(static_cast<size_t>(std::filesystem::file_size(path)), '\0');
-    file.read(&buffer.front(), buffer.size());
-    buffer.resize(file.gcount());
-    return std::istringstream(buffer);
+std::optional<std::istringstream> ReadFile(const std::filesystem::path path) {
+  std::ifstream file(path);
+  if (!file) {
+    return std::nullopt;
+  }
+  std::string buffer(static_cast<size_t>(std::filesystem::file_size(path)),
+                     '\0');
+  file.read(&buffer.front(), buffer.size());
+  buffer.resize(file.gcount());
+  return std::istringstream(buffer);
+}
+
+std::pair<std::string_view, std::string_view> split(std::string_view input,
+                                                    char splitChar) {
+  const auto split = input.find(splitChar);
+  if (split == input.npos) {
+    return {input, ""};
+  }
+  return {input.substr(0, split), input.substr(split + 1)};
 }
--- helper.hh
+++ helper.hh
@@ -1,21 +1,41 @@
 #ifndef AOC_2022_HELPER
 #define AOC_2022_HELPER
 
-#include <sstream>
+#include <algorithm>
+#include <charconv>
 #include <filesystem>
 #include <optional>
-#include <algorithm>
+#include <sstream>
+#include <type_traits>
+#include <utility>
 
 // read entire contents of a file into a string stream
 std::optional<std::istringstream> ReadFile(std::filesystem::path path);
 
 // casts int to enum, checking bad values
-template<typename Enum, Enum min, Enum max>
-constexpr std::optional<Enum> EnumCast(const int toCast) noexcept
-{
-    static_assert(min <= max, "Inverted bounds!");
-    Enum casted = std::max( min, std::min( static_cast<Enum>( toCast ), max ) );
-    return casted == toCast ? std::make_optional(casted) : std::nullopt;
+template <typename Enum, Enum min, Enum max>
+constexpr std::optional<Enum> EnumCast(const int toCast) noexcept {
+  static_assert(min <= max, "Inverted bounds!");
+  Enum casted = std::max(min, std::min(static_cast<Enum>(toCast), max));
+  return casted == toCast ? std::make_optional(casted) : std::nullopt;
 }
+
+template <typename Integer>
+std::from_chars_result from_chars(std::string_view string, Integer &result,
+                                  int base = 10) {
+  if constexpr (std::is_integral_v<Integer>) {
+    return std::from_chars(
+        string.data(), std::next(string.data(), string.size()), result, base);
+  } else if constexpr (std::is_floating_point_v<Integer>) {
+    (void)base;
+    return std::from_chars(string.data(),
+                           std::next(string.data(), string.size()), result);
+  } else {
+    static_assert(!"Must be integer or floating type");
+  }
+}
+
+std::pair<std::string_view, std::string_view> split(std::string_view input,
+                                                    char splitChar);
 
 #endif
